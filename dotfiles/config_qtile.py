17abc7164b52d101b4b9ffd530a8cb5d28bef985
Panadestein <rpana92@gmail.com>
2022-04-26 17:14:17 +0200

    Latest snapshot

--- dotfiles/config_qtile.py
+++ dotfiles/config_qtile.py
@@ -246,8 +246,8 @@ mouse = [
 
 dgroups_app_rules = []  # type: List
 follow_mouse_focus = True
-bring_front_click = False
-cursor_warp = False
+bring_front_click = True
+cursor_warp = True
 floating_layout = layout.Floating(float_rules=[
     # Run the utility of `xprop` to see the wm class and name of an X client.
     *layout.Floating.default_float_rules,
