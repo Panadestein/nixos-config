4b461e3b9d2f62e4fd8446c09fb110037f9ce0d8
Panadestein <rpana92@gmail.com>
2022-12-05 00:20:20 +0100

    Latest snapshot

---
+++ shells/fortran/shell_greenx.nix
@@ -0,0 +1,24 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  python-with-my-packages = with pkgs.python3Packages;
+    buildPythonPackage rec {
+      name = "GreenX";
+      src = ./greenX_ref/python;
+      propagatedBuildInputs = [ pytest
+                                numpy
+                                matplotlib
+                                pandas
+                                pyqt5
+                              ];
+    };
+in
+pkgs.mkShell {
+  packages = [
+    python-with-my-packages
+    pkgs.python310Packages.pytest
+    pkgs.doxygen
+    pkgs.lapack-reference
+    pkgs.cmake
+  ];
+}
+
