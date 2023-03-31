4943eeb0de214bb77a1ccff13a9e4e6ab6ef97d9
Panadestein <rpana92@gmail.com>
2023-03-31 11:33:12 +0200

    Latest Snapshot

--- systems/cyrus/configuration.nix
+++ systems/cyrus/configuration.nix
@@ -54,9 +54,6 @@
   # Load AMD CPU microcode
   hardware.cpu.amd.updateMicrocode = true;
 
-  # Add HiDPI support
-  hardware.video.hidpi.enable = true;
-
   # Kernel parameters and modules
   boot.initrd.kernelModules = [ "amdgpu" "hid-apple"];
   boot.kernelParams = [
