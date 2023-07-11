67bd164801d89662e1b24966f22853446d1b70d2
Panadestein <rpana92@gmail.com>
2023-07-11 11:49:26 +0200

    Linux 6.4 possibly broken. Roll back to 6.1

--- systems/cyrus/configuration.nix
+++ systems/cyrus/configuration.nix
@@ -49,7 +49,7 @@
   };
 
   # Use the latest linux kernel
-  boot.kernelPackages = pkgs.linuxPackages_zen;
+  boot.kernelPackages = pkgs.linuxPackages_6_1;
 
   # Load AMD CPU microcode
   hardware.cpu.amd.updateMicrocode = true;
