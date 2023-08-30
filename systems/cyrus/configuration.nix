d08425621adac40def1eb5eaaca1786d5ad9b3f2
Panadestein <rpana92@gmail.com>
2023-08-30 12:47:34 +0200

    Qtile and qt6.full broken

--- systems/cyrus/configuration.nix
+++ systems/cyrus/configuration.nix
@@ -143,7 +143,7 @@
       ];
     };
     qtile = {
-      enable = true;
+      enable = false;
     };
   };
 
@@ -235,7 +235,7 @@
     gfortran
     julia-bin
     mono
-    #qt6.full
+    # qt6.full
     (let
       my-python-packages = python-packages: with python-packages; [
         # Language server protocol
