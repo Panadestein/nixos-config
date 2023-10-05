f1d3c00805698d239c2c294cd6f67aa9c21b4590
Panadestein <rpana92@gmail.com>
2023-10-05 22:03:21 +0200

    Latest Snapshot

--- systems/cyrus/configuration.nix
+++ systems/cyrus/configuration.nix
@@ -118,13 +118,13 @@
 
   # Display manager
   services.xserver.displayManager = {
-    defaultSession = "none+qtile";
+    defaultSession = "gnome";
     gdm = {
-      enable = false;
+      enable = true;
       wayland = false;
     };
     lightdm = {
-      enable = true;
+      enable = false;
       greeters.enso = {
         enable = true;
       }; 
@@ -148,7 +148,7 @@
   };
   
   # Desktop environment
-  services.xserver.desktopManager.gnome.enable = false;
+  services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.layout = "us,de";
@@ -298,8 +298,8 @@
   services.flatpak.enable = true;
 
   # If Gnome is not installed, this must be enabled
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  #  xdg.portal.enable = true;
+  #  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 
   # Fonts
   fonts.packages = with pkgs; [
