81b2e100d1299886094c919d937e2e0e5bb12219
Panadestein <rpana92@gmail.com>
2023-10-30 11:32:53 +0100

    Latest Snapshot

--- systems/cyrus/configuration.nix
+++ systems/cyrus/configuration.nix
@@ -151,7 +151,7 @@
   };
   
   # Desktop environment
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.gnome.enable = false;
   xdg.portal.enable = lib.mkIf
     (!config.services.xserver.desktopManager.gnome.enable)
     true;
