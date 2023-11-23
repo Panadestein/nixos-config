f65bc3655ee48a0a84cd73bf4cdc352a0711a64c
Panadestein <rpana92@gmail.com>
2023-11-23 23:43:13 +0100

    Latest Snapshot

--- home/cyrus/home.nix
+++ home/cyrus/home.nix
@@ -280,6 +280,16 @@ in
       name = "Arc-Dark";
       package = pkgs.arc-theme;
     };
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
   };
 
   # Removable devices
