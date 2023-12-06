7b2bca91cdd00c9fc28bc248733d19dada92557f
Panadestein <rpana92@gmail.com>
2023-12-06 21:50:54 +0100

    Latest Snapshot

--- utils/flakes/greenx_flake.nix
+++ utils/flakes/greenx_flake.nix
@@ -8,6 +8,26 @@
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
 
+      findent = pkgs.stdenv.mkDerivation rec {
+        pname = "findent";
+        version = "4.2.6";
+        
+        src = pkgs.fetchurl {
+          url = "mirror://sourceforge/findent/${pname}-${version}.tar.gz";
+          sha512 = "1d97005a6f414a1876dd2140922125b7e399fc7b03afaea0b2fe6dfd7eb6baeb3bf16d3ae7b259e3c9a613a889f2759393c1ed5f98e5bda8ee6ea3ddd5e713c0";
+        };
+
+        nativeBuildInputs = [ pkgs.gnumake pkgs.gcc ];
+        buildInputs = [ pkgs.flex pkgs.bison ]; 
+
+        configureFlags = [ "--prefix=${placeholder "out"}" ];
+        
+        meta = {
+          homepage = "https://sourceforge.net/projects/findent/";
+          description = "A Fortran indenter";
+        };
+      };
+
       zofu = pkgs.stdenv.mkDerivation {
         pname = "zofu";
         version = "1.1.1";
@@ -94,15 +114,17 @@
             # Python packages
             custom-python-env
             # Additional libraries and utils
-            zofu
-            python310Packages.pytest
-            doxygen
-            lapack-reference
             cmake
-            valgrind
-            linuxKernel.packages.linux_zen.perf
-            hotspot
+            doxygen
+            findent
             gmp
+            hotspot
+            hyperfine
+            lapack-reference
+            linuxKernel.packages.linux_zen.perf
+            python310Packages.pytest
+            valgrind
+            zofu
           ];
           shellHook = ''
             alias val=colour-valgrind;
