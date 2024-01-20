b82d04420eee22e8e026c255b1158c5243cf8a78
Panadestein <rpana92@gmail.com>
2024-01-20 20:17:50 +0100

    Latest snapshot

--- systems/cyrus/hardware-configuration.nix
+++ systems/cyrus/hardware-configuration.nix
@@ -25,6 +25,9 @@
 
   swapDevices = [ ];
 
+  # Network
+  networking.useDHCP = lib.mkDefault true;
+
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
