363c9d1e4c50501221b1b5cb654716777c4194e6
Panadestein <rpana92@gmail.com>
2024-01-21 00:13:27 +0100

    Latest snapshot

--- systems/cyrus/configuration.nix
+++ systems/cyrus/configuration.nix
@@ -115,13 +115,13 @@
 
   # Display manager
   services.xserver.displayManager = {
-    defaultSession = "gnome";
+    defaultSession = "none+qtile";
     gdm = {
-      enable = true;
+      enable = false;
       wayland = false;
     };
     lightdm = {
-      enable = false;
+      enable = true;
       greeters.enso = {
         enable = true;
       }; 
@@ -149,7 +149,7 @@
   };
   
   # Desktop environment
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.gnome.enable = false;
   xdg.portal.enable = lib.mkIf
     (!config.services.xserver.desktopManager.gnome.enable)
     true;
