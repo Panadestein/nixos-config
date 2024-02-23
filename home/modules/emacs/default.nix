0929c5f57f1a5386f90101bb3c878177eadc2772
Panadestein <rpana92@gmail.com>
2024-02-23 15:22:37 +0100

    Latest snapshot

--- home/modules/emacs/default.nix
+++ home/modules/emacs/default.nix
@@ -5,8 +5,8 @@
     source = pkgs.fetchFromGitHub {
       owner = "Panadestein";
       repo = "emacsd";
-      rev = "bb40038e71c25e8c7d6aed4d6c642fce6d2e8b2b";
-      sha256 = "1qibnwkivhnxn4ns7rg2ljb7ys7rm9d9wzxyhhcwq5qwxg3ygwl4";
+      rev = "127291b5858a74ac6ea88f862d6ed3b7cd4933ec";
+      sha256 = "1yb4wsabvxxkk0bzhdi0pj4lh5594d78whjdawsa9bm1bfd0izpp";
     };
     recursive = true;
     onChange = builtins.readFile ../../dotfiles/set_emacs.sh;
