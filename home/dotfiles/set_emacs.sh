dc354e55e0b4eb1ac08b7f5e2518133238ff625d
Panadestein <rpana92@gmail.com>
2026-04-12 11:32:08 +0200

    Latest snapshot

--- home/dotfiles/set_emacs.sh
+++ home/dotfiles/set_emacs.sh
@@ -2,4 +2,7 @@
 
 cd "$HOME"/.emacs.d
 mkdir -p ./lib ./snippets/irp-mode ./undo
-emacs --batch --eval "(require 'org-loaddefs)" --eval '(org-babel-tangle-file "./content/index.org")'
+emacs --batch \
+      --eval "(require 'org-loaddefs)" \
+      --eval "(setq org-resource-download-policy t)" \
+      --eval '(org-babel-tangle-file "./content/index.org")'
