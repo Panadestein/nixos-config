11488c2c894dae21aebe3cd9cb95a635c87ca940
Panadestein <rpana92@gmail.com>
2026-05-08 11:57:53 +0200

    Latest snapshot

--- flake.nix
+++ flake.nix
@@ -68,6 +68,7 @@
               home-manager.nixosModules.home-manager
               {
                 home-manager.useGlobalPkgs = true;
+                home-manager.extraSpecialArgs = { inherit inputs; };
                 home-manager.users.${persona} = ./home/${rechnerNixOS}/home.nix;
               }
             ];
