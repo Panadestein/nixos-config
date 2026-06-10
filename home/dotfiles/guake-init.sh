75b4eb2590f410f0e53ef0f069285d760970fa16
Panadestein <rpana92@gmail.com>
2026-06-10 15:37:29 +0200

    Latest snapshot

--- home/dotfiles/guake-init.sh
+++ home/dotfiles/guake-init.sh
@@ -3,6 +3,12 @@
 # Make sure BQN symbols are activated
 setxkbmap -layout us,bqn -option grp:switch
 
+# Fish
+FISH_CMD="clear; figlet 'Fish' | command lolcat; exec fish"
+guake --select-tab=0
+guake --rename-current-tab="Fish"
+guake --execute-command="$FISH_CMD"
+
 # BQN
 BQN_CMD="clear; figlet 'BQN' | command lolcat; cbqn --version; exec cbqn"
 guake --new-tab=. --rename-current-tab="BQN" --execute-command="$BQN_CMD"
@@ -10,6 +16,10 @@ guake --new-tab=. --rename-current-tab="BQN" --execute-command="$BQN_CMD"
 # IPython
 IPY_CMD="clear; figlet 'IPython' | command lolcat; exec ipython"
 guake --new-tab=. --rename-current-tab="IPython" --execute-command="$IPY_CMD"
+
+# Numbat
+NBT_CMD="clear; figlet 'Numbat' | command lolcat; exec numbat --intro-banner off"
+guake --new-tab=. --rename-current-tab="Numbat" --execute-command="$NBT_CMD"
 
 # Cleanup
 guake --select-tab=0
