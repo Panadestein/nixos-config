c29bc44793a3873f03cc506734595b2df2d7dab3
Panadestein <rpana92@gmail.com>
2026-08-17 09:37:47 +0200

    Latest snapshot

--- home/cyrus/home.nix
+++ home/cyrus/home.nix
@@ -155,7 +155,7 @@ in
     firefox
     google-chrome
     localsend
-    signal
+    signal-desktop
     (slack.overrideAttrs (old: {
       postFixup = (old.postFixup or "") + ''
         substituteInPlace $out/share/applications/slack.desktop \
