1a36c0e0ff2658b8acf8ec9a5f96f320db796713
Panadestein <rpana92@gmail.com>
2026-09-21 00:01:53 +0200

    refact: better starship prompt

--- home/modules/starship/default.nix
+++ home/modules/starship/default.nix
@@ -12,9 +12,11 @@ in
     enableZshIntegration = true;
 
     settings = {
-      add_newline = false;
+      add_newline = true;
+      command_timeout = 200;
+      format = "[$directory$git_branch$git_status]($style)$character";
+
       palette = "oehme";
-      format = "$directory$git_branch$git_status$nix_shell$cmd_duration$line_break$character";
 
       palettes.oehme = {
         blue = theme.blue;
@@ -27,36 +29,37 @@ in
         yellow = theme.yellow;
       };
 
-      directory = {
-        format = "[$path]($style)[$read_only]($read_only_style) ";
-        style = "bold blue";
-        read_only = " ro";
-        read_only_style = "bold red";
-        truncation_length = 4;
-        truncate_to_repo = false;
-      };
-      git_branch = {
-        format = "[$symbol$branch(:$remote_branch)]($style) ";
-        symbol = "git:";
-        style = "bold magenta";
-      };
-      git_status = {
-        format = "([$all_status$ahead_behind]($style) )";
-        style = "bold yellow";
-      };
-      nix_shell = {
-        format = "[$symbol$name]($style) ";
-        symbol = "nix:";
-        style = "bold cyan";
-      };
-      cmd_duration = {
-        min_time = 2000;
-        format = "[$duration]($style) ";
-        style = "bold orange";
-      };
       character = {
-        success_symbol = "[❯](bold green) ";
-        error_symbol = "[❯](bold red) ";
+        error_symbol = "[✗](bold cyan)";
+        success_symbol = "[❯](bold cyan)";
+      };
+
+      directory = {
+        truncation_length = 2;
+        truncation_symbol = "…/";
+        repo_root_style = "bold cyan";
+        repo_root_format = "[$repo_root]($repo_root_style)[$path]($style)[$read_only]($read_only_style) ";
+      };
+
+      git_branch = {
+        format = "[$branch]($style) ";
+        style = "italic cyan";
+      };
+
+      git_status = {
+        format = "[$all_status]($style)";
+        style = "cyan";
+        ahead = "⇡\${count} ";
+        diverged = "⇕⇡\${ahead_count}⇣\${behind_count} ";
+        behind = "⇣\${count} ";
+        conflicted = " ";
+        up_to_date = " ";
+        untracked = "? ";
+        modified = " ";
+        stashed = "";
+        staged = "";
+        renamed = "";
+        deleted = "";
       };
     };
   };
