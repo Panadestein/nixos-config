8886447fe4e39c6ecffccc8a4d74e1e7b270ba72
Panadestein <rpana92@gmail.com>
2026-09-22 00:14:18 +0200

    chore: config clean up

--- systems/cyrus/configuration.nix
+++ systems/cyrus/configuration.nix
@@ -155,9 +155,6 @@ in
 
   # Network configuration
   networking = {
-    # The router assigned by DHCP currently does not resolve public names.
-    # These are also needed on a first boot, when Emacs bootstraps straight.el
-    # and Neovim installs its vim-plug-managed plugins.
     nameservers = [
       "1.1.1.1"
       "9.9.9.9"
